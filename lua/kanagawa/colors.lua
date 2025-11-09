be4545aeff366528c08d9510879f7d7a2523d6e4
JoshuaLim25 <chillythermometer@gmail.com>
2025-11-08 20:32:33 -0500

    init commit

--- lua/kanagawa/colors.lua
+++ lua/kanagawa/colors.lua
@@ -1,63 +1,64 @@
+local M = {}
+
 ---@class PaletteColors
 local palette = {
-
-    -- Bg Shades
-    sumiInk0 = "#16161D",
-    sumiInk1 = "#181820",
-    sumiInk2 = "#1a1a22",
-    sumiInk3 = "#1F1F28",
-    sumiInk4 = "#2A2A37",
-    sumiInk5 = "#363646",
-    sumiInk6 = "#54546D", --fg
+    -- Bg Shades (Tomorrow Night)
+    sumiInk0 = "#1d1f21",
+    sumiInk1 = "#1d1f21",
+    sumiInk2 = "#1d1f21",
+    sumiInk3 = "#1d1f21",
+    sumiInk4 = "#282a2e",
+    sumiInk5 = "#373b41",
+    sumiInk6 = "#707880",
 
     -- Popup and Floats
-    waveBlue1 = "#223249",
-    waveBlue2 = "#2D4F67",
+    waveBlue1 = "#2D4F67",
+    waveBlue2 = "#223249",
 
     -- Diff and Git
     winterGreen = "#2B3328",
     winterYellow = "#49443C",
     winterRed = "#43242B",
     winterBlue = "#252535",
-    autumnGreen = "#76946A",
-    autumnRed = "#C34043",
-    autumnYellow = "#DCA561",
+    autumnGreen = "#b5bd68",
+    autumnRed = "#cc6666",
+    autumnYellow = "#f0c674",
 
     -- Diag
-    samuraiRed = "#E82424",
-    roninYellow = "#FF9E3B",
-    waveAqua1 = "#6A9589",
-    dragonBlue = "#658594",
+    samuraiRed = "#cc6666",
+    roninYellow = "#f0c674",
+    waveAqua1 = "#7AA89F",
+    dragonBlue = "#81a2be",
 
     -- Fg and Comments
-    oldWhite = "#C8C093",
-    fujiWhite = "#DCD7BA",
-    fujiGray = "#727169",
+    oldWhite = "#707880",
+    fujiWhite = "#c5c8c6",
+    fujiGray = "#707880",
 
-    oniViolet = "#957FB8",
-    oniViolet2 = "#b8b4d0",
-    crystalBlue = "#7E9CD8",
-    springViolet1 = "#938AA9",
-    springViolet2 = "#9CABCA",
-    springBlue = "#7FB4CA",
-    lightBlue = "#A3D4D5", -- unused yet
-    waveAqua2 = "#7AA89F", -- improve lightness: desaturated greenish Aqua
+    oniViolet = "#b294bb",
+    oniViolet2 = "#b294bb",
+    crystalBlue = "#81a2be",
+    springViolet1 = "#b294bb",
+    springViolet2 = "#9fb5c9",
+    springBlue = "#9fb5c9",
+    lightBlue = "#81a2be",
+    waveAqua2 = "#87a987",
 
-    -- waveAqua2  = "#68AD99",
-    -- waveAqua4  = "#7AA880",
-    -- waveAqua5  = "#6CAF95",
-    -- waveAqua3  = "#68AD99",
+    -- Main colors
+    waveRed = "#cc6666",
+    surimiOrange = "#de935f",
+    carpYellow = "#f0c674",
+    springGreen = "#b5bd68",
+    sakuraPink = "#c4746e",
 
-    springGreen = "#98BB6C",
-    boatYellow1 = "#938056",
-    boatYellow2 = "#C0A36E",
-    carpYellow = "#E6C384",
-
-    sakuraPink = "#D27E99",
-    waveRed = "#E46876",
-    peachRed = "#FF5D62",
-    surimiOrange = "#FFA066",
-    katanaGray = "#717C7C",
+    -- Dragon theme
+    dragonInk0 = "#0d0c0c",
+    dragonInk1 = "#12120f",
+    dragonInk2 = "#1D1C19",
+    dragonInk3 = "#181616",
+    dragonInk4 = "#282727",
+    dragonInk5 = "#393836",
+    dragonInk6 = "#625e5a",
 
     dragonBlack0 = "#0d0c0c",
     dragonBlack1 = "#12120f",
@@ -67,29 +68,30 @@ local palette = {
     dragonBlack5 = "#393836",
     dragonBlack6 = "#625e5a",
 
-    dragonWhite = "#c5c9c5",
-    dragonGreen = "#87a987",
-    dragonGreen2 = "#8a9a7b",
-    dragonPink = "#a292a3",
-    dragonOrange = "#b6927b",
-    dragonOrange2 = "#b98d7b",
-    dragonGray = "#a6a69c",
-    dragonGray2 = "#9e9b93",
-    dragonGray3 = "#7a8382",
-    dragonBlue2 = "#8ba4b0",
-    dragonViolet= "#8992a7",
-    dragonRed = "#c4746e",
-    dragonAqua = "#8ea4a2",
-    dragonAsh = "#737c73",
-    dragonTeal = "#949fb5",
-    dragonYellow = "#c4b28a",--"#a99c8b",
-    -- "#8a9aa3",
+    dragonWhite = "#c5c8c6",
+    dragonGreen = "#b5bd68",
+    dragonGreen2 = "#87a987",
+    dragonPink = "#c4746e",
+    dragonOrange = "#de935f",
+    dragonOrange2 = "#de935f",
+    dragonGray = "#707880",
+    dragonGray2 = "#707880",
+    dragonGray3 = "#373b41",
+    dragonBlue2 = "#81a2be",
+    dragonViolet = "#b294bb",
+    dragonRed = "#cc6666",
+    dragonAqua = "#87a987",
+    dragonAsh = "#373b41",
+    dragonTeal = "#7AA89F",
+    dragonYellow = "#f0c674",
 
-    lotusInk1 = "#545464",
+    -- Lotus theme
+    lotusInk0 = "#545454",
+    lotusInk1 = "#545454",
     lotusInk2 = "#43436c",
-    lotusGray = "#dcd7ba",
-    lotusGray2 = "#716e61",
-    lotusGray3 = "#8a8980",
+    lotusGray = "#707880",
+    lotusGray2 = "#707880",
+    lotusGray3 = "#707880",
     lotusWhite0 = "#d5cea3",
     lotusWhite1 = "#dcd5ac",
     lotusWhite2 = "#e5ddb0",
@@ -105,44 +107,38 @@ local palette = {
     lotusBlue3 = "#9fb5c9",
     lotusBlue4 = "#4d699b",
     lotusBlue5 = "#5d57a3",
-    lotusGreen = "#6f894e",
-    lotusGreen2 = "#6e915f",
-    lotusGreen3 = "#b7d0ae",
-    lotusPink = "#b35b79",
-    lotusOrange = "#cc6d00",
-    lotusOrange2 = "#e98a00",
-    lotusYellow = "#77713f",
-    lotusYellow2 = "#836f4a",
-    lotusYellow3 = "#de9800",
-    lotusYellow4 = "#f9d791",
-    lotusRed = "#c84053",
-    lotusRed2 = "#d7474b",
-    lotusRed3 = "#e82424",
-    lotusRed4 = "#d9a594",
-    lotusAqua = "#597b75",
-    lotusAqua2 = "#5e857a",
-    lotusTeal1 = "#4e8ca2",
-    lotusTeal2 = "#6693bf",
-    lotusTeal3 = "#5a7785",
-    lotusCyan = "#d7e3d8",
+    lotusGreen = "#b5bd68",
+    lotusGreen2 = "#87a987",
+    lotusGreen3 = "#87a987",
+    lotusPink = "#c4746e",
+    lotusOrange = "#de935f",
+    lotusOrange2 = "#de935f",
+    lotusYellow = "#f0c674",
+    lotusYellow2 = "#f0c674",
+    lotusYellow3 = "#f0c674",
+    lotusYellow4 = "#f0c674",
+    lotusRed = "#cc6666",
+    lotusRed2 = "#cc6666",
+    lotusRed3 = "#cc6666",
+    lotusRed4 = "#cc6666",
+    lotusAqua = "#87a987",
+    lotusAqua2 = "#7AA89F",
+    lotusTeal1 = "#7AA89F",
+    lotusTeal2 = "#7AA89F",
+    lotusTeal3 = "#7AA89F",
+    lotusCyan = "#81a2be",
 }
 
-local M = {}
---- Generate colors table:
---- * opts:
----   - colors: Table of personalized colors and/or overrides of existing ones.
----     Defaults to KanagawaConfig.colors.
----   - theme: Use selected theme. Defaults to KanagawaConfig.theme
----     according to the value of 'background' option.
----@param opts? { colors?: table, theme?: string }
----@return { theme: ThemeColors, palette: PaletteColors}
+--- Generate colors based on the provided palette and config options
+--- @param opts? { colors?: table, theme?: string }
+--- @return { theme: ThemeColors, palette: PaletteColors}
 function M.setup(opts)
     opts = opts or {}
     local override_colors = opts.colors or require("kanagawa").config.colors
-    local theme = opts.theme or require("kanagawa")._CURRENT_THEME -- WARN: this fails if called before kanagawa.load()
+    local theme = opts.theme or require("kanagawa")._CURRENT_THEME
 
     if not theme then
-        error("kanagawa.colors.setup(): Unable to infer `theme`. Either specify a theme or call this function after ':colorscheme kanagawa'")
+        theme = vim.o.background == "light" and "lotus" or "wave"
     end
 
     -- Add to and/or override palette_colors
@@ -151,15 +147,27 @@ function M.setup(opts)
     -- Generate the theme according to the updated palette colors
     local theme_colors = require("kanagawa.themes")[theme](updated_palette_colors)
 
-    -- Add to and/or override theme_colors
-    local theme_overrides = vim.tbl_deep_extend("force", override_colors.theme["all"] or {}, override_colors.theme[theme] or {} )
-    local updated_theme_colors = vim.tbl_deep_extend("force", theme_colors, theme_overrides)
-    -- return palette_colors AND theme_colors
+    -- Add to and/or override theme_colors  
+    local function nested_extend(base, override)
+        for k, v in pairs(override) do
+            if type(v) == "table" and type(base[k]) == "table" then
+                nested_extend(base[k], v)
+            else
+                base[k] = v
+            end
+        end
+    end
 
-    return {
-        theme = updated_theme_colors,
-        palette = updated_palette_colors,
-    }
+    local override_theme_colors = override_colors.theme
+    if override_theme_colors and override_theme_colors[theme] then
+        nested_extend(theme_colors, override_theme_colors[theme])
+    end
+
+    if override_theme_colors and override_theme_colors.all then
+        nested_extend(theme_colors, override_theme_colors.all)
+    end
+
+    return { palette = updated_palette_colors, theme = theme_colors }
 end
 
 return M
